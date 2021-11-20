bbe4504d7a5e7cdc5e922f5a437025ce1398e517
Ricardo de Souza Lucas <ricardo.lucas.e@midway.com.br>
2021-11-20 11:52:47 -0300

    gerar arquivo csv a partir de lista de documentos; estrutuda da lista de documentos no readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Computação Cognitiva
+NLP Formato da Lista de documentos para geração do CSV:
+
+````
+[{
+    'token': 'testtoken',
+    'tf': 'testtf',
+    'df': 'testdf',
+    'idf': 'testidf',
+    'tf-idf': 'testtf-idf'
+}, {
+    'token': 'testtoken',
+    'tf': 'testtf',
+    'df': 'testdf',
+    'idf': 'testidf',
+    'tf-idf': 'testtf-idf'
+}]
--- src/nlp_proc.py
+++ src/nlp_proc.py
@@ -1,28 +1,44 @@
 from src.gen_function import contadorDeStrigs
+import csv
+
 
 def pdf_to_list():
     result = ''
     return result
 
+
 def stop_words():
     result = ''
     return result
+
 
 def tokenizar():
     result = ''
     return result
 
+
 def lematizar():
     result = ''
     return result
+
 
 def gerar_resultados():
     result = ''
     return result
 
-def gerar_csv():
-    result = ''
+
+def gerar_csv(document_list: list):
+    # csv header
+    fieldnames = ['Token', 'TF', 'DF', 'IDF', 'TF-IDF']
+
+    with open('countries.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(document_list)
+
+    result = 'documents.csv'
     return result
+
 
 def gerar_mapa_palavras():
     result = ''
