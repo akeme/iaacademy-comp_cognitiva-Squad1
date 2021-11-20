ce6f41dd5714bfdb245656ceba17ebbe53972fe1
Marcio Pinheiro Neris <marcio.neris@gmail.com>
2021-11-20 11:30:30 -0300

    definição dos nomes das funções

---
+++ src/nlp_proc.py
@@ -0,0 +1,29 @@
+from src.gen_function import contadorDeStrigs
+
+def pdf_to_list():
+    result = ''
+    return result
+
+def stop_words():
+    result = ''
+    return result
+
+def tokenizar():
+    result = ''
+    return result
+
+def lematizar():
+    result = ''
+    return result
+
+def gerar_resultados():
+    result = ''
+    return result
+
+def gerar_csv():
+    result = ''
+    return result
+
+def gerar_mapa_palavras():
+    result = ''
+    return result
