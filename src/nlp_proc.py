b927dc7503aeb617cef586be034fda2681aeb90b
MichaelNascimento <diazmichael07@gmail.com>
2021-11-20 16:02:12 -0300

    criação da função pdf_to_list

--- src/nlp_proc.py
+++ src/nlp_proc.py
@@ -1,11 +1,25 @@
-from src.gen_function import contadorDeStrigs
+from PyPDF2 import PdfFileReader
 import csv
 
+def pdf_to_list(caminho_pdf):
+    """
+        função que extrai as palavras de um pdf
+        :param caminho_pdf: caminho do pdf para a extração das palavras
+        :return: retorna uma lista com as palavras extraidas do pdf
+        """
+    # abre o pdf
+    pdf = PdfFileReader(open(caminho_pdf, "rb"))
 
-def pdf_to_list():
-    result = ''
-    return result
+    # inicializa lista com as paginas
+    lista_paginas = []
 
+    for page in range(0, pdf.getNumPages()):
+        pagina_atual = pdf.getPage(page)
+        texto = pagina_atual.extractText()
+        palavras_pagina = texto.split()
+        lista_paginas.append(palavras_pagina)
+
+    return lista_paginas
 
 def stop_words():
     result = ''
